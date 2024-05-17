6d43d906df4fbf8865c542aaf9c2c8d52c691ad4
“mohitdudhatgit config --global user.name “mohitdudhat <keshasingala04@gmail.com>
2024-05-17 18:34:25 +0530

    modified file

--- src/Pages/User/Homepage.jsx
+++ src/Pages/User/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
+import Carousel from '../../'
 
 const Homepage = () => {
   return (
     <>
-      <Header/>  
+      <Header/>
+      
     </>
   )
 }
